2e957aa416bc06aae427a1e1c7a852e3539a7e1d
Hleb Sharstuk <sharhleb@gmail.com>
2022-02-04 13:06:49 +0300

    feat(US05): add template, stylesheets, router Links for admin layout

--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+
+import { ROUTE_CONFIGS } from "../../../../utils/constants/route.consts";
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.scss']
 })
-export class AdminLayoutComponent { }
+export class AdminLayoutComponent {
+
+  dashLink: any[] = [
+    ROUTE_CONFIGS['adminPage'].fullpath,
+    ROUTE_CONFIGS['adminDashboard'].path
+  ];
+
+  createLink: any[] = [
+    ROUTE_CONFIGS['adminPage'].fullpath,
+    ROUTE_CONFIGS['adminCreate'].path
+  ];
+
+  loginLink: any[] = [
+    ROUTE_CONFIGS['adminPage'].fullpath,
+    ROUTE_CONFIGS['adminLogin'].path
+  ];
+
+  constructor(private router: Router) {
+  }
+
+  logout(event: Event) {
+    event.preventDefault();
+    this.router.navigate(this.loginLink);
+  }
+}
