1d0684649727dd5b88284b17ddb9d799bced0b5d
Hleb Sharstuk <sharhleb@gmail.com>
2022-02-04 10:25:17 +0300

    feat(US02): add routes and components for admin layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,21 @@ import { RouterModule, Routes } from '@angular/router';
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {PostPageComponent} from './post-page/post-page.component';
+
 import {ROUTE_CONFIGS} from './utils/constants/route.consts';
 
 const routes: Routes = [
-  {path: ROUTE_CONFIGS['home'].path, component: MainLayoutComponent, children: [
+  {
+    path: ROUTE_CONFIGS['home'].path, component: MainLayoutComponent, children: [
       {path: ROUTE_CONFIGS['home'].path, redirectTo: '/', pathMatch: 'full'},
       {path: ROUTE_CONFIGS['home'].path, component: HomePageComponent},
       {path: ROUTE_CONFIGS['postPage'].path, component: PostPageComponent},
-    ]}
+    ]
+  },
+  {
+    path: ROUTE_CONFIGS['adminPage'].path,
+    loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule)
+  }
 ];
 
 @NgModule({
--- src/app/utils/constants/route.consts.ts
+++ src/app/utils/constants/route.consts.ts
@@ -10,7 +10,13 @@ const postPage: RouteConfig = {
   path: 'post/:id'
 }
 
+const adminPage: RouteConfig = {
+  name: 'admin-page',
+  path: 'admin'
+}
+
 export const ROUTE_CONFIGS: RouteConfigs = {
   home,
   postPage,
+  adminPage,
 }
