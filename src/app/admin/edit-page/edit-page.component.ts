aef5c5e3d277a2a731029f59a6fc80a10e8e5c06
Hleb Sharstuk <sharhleb@gmail.com>
2022-02-09 17:05:29 +0300

    feat(US15): add template and base form logic for EditPage

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { PostsService } from '../../shared/components/posts.service';
+import { Subscription, switchMap } from 'rxjs';
+import { FormConfigs, Post } from '../../utils/interfaces/admin-panel.interfaces';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FIELD_FORM_CONSTS } from '../../utils/constants/form.consts';
 
 @Component({
   selector: 'app-edit-page',
@@ -6,4 +13,40 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./edit-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EditPageComponent { }
+export class EditPageComponent implements OnInit, OnDestroy{
+  public subscription!: Subscription;
+  public form!: FormGroup;
+  public fieldFormConsts: FormConfigs = FIELD_FORM_CONSTS;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService,
+    private cd: ChangeDetectorRef,
+  ) { }
+
+  ngOnInit(): void {
+    this.subscription = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postsService.getById(params['id']);
+      }))
+      .subscribe( (post: Post) => {
+        this.form = new FormGroup({
+          title: new FormControl(post.title, Validators.required),
+          text: new FormControl(post.text, Validators.required)
+        })
+        this.cd.markForCheck();
+      })
+  }
+
+  validCheck(fieldStr: string): boolean | undefined {
+    return (this.form.get(fieldStr)?.touched && this.form.get(fieldStr)?.invalid)
+  }
+
+  submit() {
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+}
--- src/app/shared/components/posts.service.ts
+++ src/app/shared/components/posts.service.ts
@@ -36,4 +36,15 @@ export class PostsService {
           }))
       }))
   }
+
+  getById(id: string): Observable<Post> {
+    return this.http.get<Post>(`${environment.fbDbUrl}/posts/${id}.json`)
+      .pipe(map((post: Post | any) => {
+        return {
+          ...post,
+          id,
+          date: new Date(post.date)
+        }
+      }))
+  }
 }
