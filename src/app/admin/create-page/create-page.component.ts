8757611d91fd70e61eb712f72b7e13d9e53d4768
Hleb Sharstuk <sharhleb@gmail.com>
2022-02-09 14:24:02 +0300

    fix(US15): add unsubscribe in CreatePageComponent

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { FormConfigs, Post } from '../../utils/interfaces/admin-panel.interfaces';
 import { PostsService } from '../../shared/components/posts.service';
 import { FIELD_FORM_CONSTS } from '../../utils/constants/form.consts';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create-page',
@@ -11,8 +12,9 @@ import { FIELD_FORM_CONSTS } from '../../utils/constants/form.consts';
   styleUrls: ['./create-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CreatePageComponent implements OnInit{
+export class CreatePageComponent implements OnInit, OnDestroy{
   public form!: FormGroup;
+  public postCreateSub!: Subscription;
   public fieldFormConsts: FormConfigs = FIELD_FORM_CONSTS;
 
   constructor(private readonly postsService: PostsService) {
@@ -30,13 +32,13 @@ export class CreatePageComponent implements OnInit{
     if (this.form.invalid) { return }
 
     const post: Post = {
-      title: this.form.value.title,
-      author: this.form.value.author,
-      text: this.form.value.text,
+      title: this.form?.value?.title,
+      author: this.form?.value?.author,
+      text: this.form?.value?.text,
       date: new Date()
     }
 
-    this.postsService.create(post).subscribe(() => {
+    this.postCreateSub = this.postsService.create(post).subscribe(() => {
       this.form.reset();
     })
   }
@@ -44,6 +46,11 @@ export class CreatePageComponent implements OnInit{
   validCheck(fieldStr: string): boolean | undefined {
     return (this.form.get(fieldStr)?.touched && this.form.get(fieldStr)?.invalid)
   }
+
+  ngOnDestroy() {
+    this.postCreateSub.unsubscribe();
+  }
+
 }
 
 
